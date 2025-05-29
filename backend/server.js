3b448914bf00d0354cc572c4398f4f3a4b339684
אלירן דנינו <danino93@gmail.com>
2025-05-29 11:03:05 +0300

    תיקון CORS: הוספת X-Requested-With והסרת credentials מיותרים

--- backend/server.js
+++ backend/server.js
@@ -48,8 +48,7 @@ app.use(cors({
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 
 app.use(express.json());
